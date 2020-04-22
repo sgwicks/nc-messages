021900d69cd71651e311a55c816f1298702e9c17
jakedheath123 <jakedheath@gmail.com>
2020-04-22 14:10:32 +0100

    import ChatHistory component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Users from "./components/Users";
+import ChatHistory from "./components/ChatHistory";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Users />
+      <ChatHistory />
     </div>
   );
 }
