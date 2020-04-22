0b28235b7724956a7d2b83bfbcc266c8ddcf02f7
sgwicks <samgwicks@gmail.com>
2020-04-22 12:55:31 +0100

    Set up route, controller and model for users/:user_id, no functionality

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const usersRouter = require('./routes/users.js');
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html'); // whatever frontend come up with
 });
+
+app.use('/users', usersRouter);
 
 io.on('connection', (socket) => {
   console.log('a user connected');
@@ -14,3 +17,5 @@ io.on('connection', (socket) => {
 http.listen(9090, () => {
   console.log('listening on port 9090');
 });
+
+module.exports = app;
