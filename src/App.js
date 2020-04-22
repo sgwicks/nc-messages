0c13b4d2996ac047b34e4882f60fbff458efd9e2
jakedheath123 <jakedheath@gmail.com>
2020-04-22 12:19:58 +0100

    import users component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Users from "./components/Users";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Users />
     </div>
   );
 }
